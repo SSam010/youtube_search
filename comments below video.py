2c79cda89c688db60e9f8ba13f4aa4ffcc336723
SSam010 <119804759+SSam010@users.noreply.github.com>
2022-12-10 20:49:21 +0400

    Download first files

---
+++ comments below video.py
@@ -0,0 +1,41 @@
+import json
+import os
+
+from youtubesearchpython import *
+
+
+def comment_getting():
+    with open(name_dir_video_comm + '/comments.json', 'w') as vh:
+        json.dump(comments.comments['result'], vh, indent=4)
+
+
+channel_directory = input('write directory address: ')
+
+# getting comments below the video from video information from the selected direction
+for video_of_selecting_channel in os.listdir(channel_directory):
+    if ".json" in video_of_selecting_channel:
+        name_dir_video = channel_directory + '/' + video_of_selecting_channel
+
+        with open(name_dir_video, "r") as vch:
+
+            data_video = json.load(vch)
+            id_video_channel = data_video['id']
+
+            # creating directory video comments
+            name_dir_video_comm = channel_directory + id_video_channel + '/comments '
+
+            try:
+                # getting comments from video
+                comments = Comments(id_video_channel)
+
+                if name_dir_video_comm not in channel_directory:
+                    os.mkdir(name_dir_video_comm)
+
+                comment_getting()
+
+                # check more comments in channel (default limit 20)
+                while comments.hasMoreComments:
+                    comments.getNextComments()
+                    comment_getting()
+            finally:
+                continue
---
+++ youtube_search_info.py
@@ -0,0 +1,25 @@
+import json
+import os
+from datetime import datetime
+from youtubesearchpython import VideosSearch
+
+# time now
+time_now = datetime.now()
+time_str = time_now.strftime("%d-%m-%Y-%H-%M-%S")
+name_dir = 'search_result ' + time_str
+
+
+# creating directory with name "name_dir"
+os.mkdir(name_dir)
+
+my_video_search = VideosSearch(input('Write video name: '), limit=int(input("Write number of search results: ")))
+answer = my_video_search.result()
+
+# writing the result to json
+for number in answer['result']:
+    number['Time search'] = str(time_now)
+    with open('./' + name_dir + '/' + str(number['id']) + '.json', 'w') as f:
+        json.dump(number, f, indent=4)
+
+print("\nThe search result is placed in the directory: " + name_dir)
+
