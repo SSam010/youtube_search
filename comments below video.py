f236a45b9a0a95a81fbe2bc2603ffe5980fc8b75
SSam010 <119804759+SSam010@users.noreply.github.com>
2022-12-15 21:26:25 +0400

    Исправил работу с папками

--- comments below video.py
+++ comments below video.py
@@ -9,20 +9,21 @@ def comment_getting():
         json.dump(comments.comments['result'], vh, indent=4)
 
 
-channel_directory = input('write directory address: ')
-
+search_dir = input('write directory address: ')
 # getting comments below the video from video information from the selected direction
-for video_of_selecting_channel in os.listdir(channel_directory):
-    if ".json" in video_of_selecting_channel:
-        name_dir_video = channel_directory + '/' + video_of_selecting_channel
+for i in os.listdir(search_dir):
+    channel_directory = search_dir + '/' + i
+    for video_of_selecting_channel in os.listdir(channel_directory):
+        if ".json" in video_of_selecting_channel:
+            name_dir_video = channel_directory + '/' + video_of_selecting_channel
 
-        with open(name_dir_video, "r") as vch:
+            with open(name_dir_video, "r") as vch:
 
-            data_video = json.load(vch)
-            id_video_channel = data_video['id']
+                data_video = json.load(vch)
+                id_video_channel = data_video['id']
 
-            # creating directory video comments
-            name_dir_video_comm = channel_directory + id_video_channel + '/comments '
+                # creating directory video comments
+                name_dir_video_comm = channel_directory + '/' + id_video_channel
 
             try:
                 # getting comments from video
