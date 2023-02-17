135cd322f3973999fb61a18015377dcb9c5a48a5
SSam010 <119804759+SSam010@users.noreply.github.com>
2023-02-17 20:17:06 +0400

    Отредактирован файл parcer.py

--- Additionally/parser.py
+++ Additionally/parser.py
@@ -4,6 +4,7 @@ from parsing_to_sql import comments_below_video as cbv
 from parsing_to_sql import transfer_data_to_SQL as tds
 from parsing_to_sql import youtube_video_from_chanel_v2 as yvc
 
+# Creating directory name and getting time
 time_now = datetime.now()
 time_str = time_now.strftime("%d-%m-%Y-%H-%M-%S")
 dir_search = 'search_result ' + time_str
@@ -16,11 +17,15 @@ SETTINGS = {
 
 # Getting video data
 yvc.start_parsing_video(list_channel_id=SETTINGS['file_txt_address'],
-                        dir_search=SETTINGS['search_directory'], time_now=time_now)
+                        dir_search=SETTINGS['search_directory'],
+                        time_now=time_now
+                        )
 
 # Getting comments from a video
 cbv.start_parsing_comments(search_dir=SETTINGS['search_directory'])
 
 # Sending to SQLLite
 tds.transfer(list_channel_id=SETTINGS['file_txt_address'],
-             dir_search=SETTINGS['search_directory'], db_address=SETTINGS['db_address'])
+             dir_search=SETTINGS['search_directory'],
+             db_address=SETTINGS['db_address']
+             )
